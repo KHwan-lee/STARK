0079b1b545bf2a1b13abfa03f76a8f57edce777b
KHwan-lee <115637946+KHwan-lee@users.noreply.github.com>
2026-01-14 16:40:46 +0900

    Delete WindowsPowerShell directory

--- WindowsPowerShell/profile.ps1
+++
@@ -1,8 +0,0 @@
-
-#region conda initialize
-# !! Contents within this block are managed by 'conda init' !!
-If (Test-Path "C:\Program Files\anaconda\Scripts\conda.exe") {
-    (& "C:\Program Files\anaconda\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
-}
-#endregion
-
